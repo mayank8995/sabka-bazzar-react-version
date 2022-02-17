3e224ac20d408957a3c4e114dda131fcb6a99150
Mayank Gupta <mayank.gupta1@publicissapient.com>
2022-02-17 14:40:51 +0530

    lazy loading image using intersection observer

--- src/components/LazyLoadImage/LazyLoader.js
+++ src/components/LazyLoadImage/LazyLoader.js
@@ -1,23 +1,34 @@
-import React,{useRef, useEffect} from 'react';
+import React, { useRef, useEffect } from 'react';
 
-const LazyLoader = ({src,threshold, alt, width, height}) =>{
+const LazyLoader = ({ src, threshold, alt, width, height }) => {
     const ref = useRef(null);
     const io = useRef(null);
-    useEffect(()=>{
-        if(ref.current){
-            io.current = new IntersectionObserver((entries)=>{
-                entries.forEach((entry) => {
-                    if(entry.intersectionRatio > 0.5){
-                        ref.current.src = src
-                        io.current.unobserver(ref.current)
-                    }
-                })
-            },{threshold})
+
+    useEffect(() => {
+        if (ref.current) {
+            io.current = new IntersectionObserver(
+                entries => {
+                    entries.forEach(entry => {
+                        if (entry.intersectionRatio > 0.5) {
+                            ref.current.src = src;
+
+                            io.current.unobserve(ref.current);
+                        }
+                    });
+                },
+                {
+                    threshold
+                }
+            );
+
             io.current.observe(ref.current);
         }
-        return () => io.current.unobserver(ref.current)
-    },[ref])
-    return <img ref={ref} alt={alt} width={width} height={height}/>
+        return () => {
+            io.current.unobserve(ref.current);
+        };
+    }, [ref]);
+
+    return <img ref={ref} width={width} height={height} alt={alt} />;
 
 }
 export default LazyLoader;
--- src/components/category/category.jsx
+++ src/components/category/category.jsx
@@ -8,7 +8,7 @@ const Category = ({ categoryData }) => {
             if (index % 2 === 0) {
                 return <div key={item.key} id={item.id} className='category'>
                     <div className='i'>
-                        <LazyLoader src={item.imageUrl} threshold={[0, 0.5, 1]} alt={item.name} width="300" height="185" />
+                        <LazyLoader src={item.imageUrl} threshold={[0, 0.25,0.5, 1]} alt={item.name} width="300" height="185" />
                     </div>
                     <div className='description'>
                         <p>{item.description}</p>
